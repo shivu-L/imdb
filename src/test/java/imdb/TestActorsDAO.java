40019e4389f9be847a8fd7f78ce25a415ccea3d6
Vaibhav <Vaibhav@ISTAR-DEV-2>
2018-08-14 20:02:14 +0530

    Test classes added for all DAOs

---
+++ src/test/java/imdb/TestActorsDAO.java
@@ -0,0 +1,28 @@
+package imdb;
+
+import org.hibernate.Session;
+
+import dao.ActorsDAO;
+import dao.DirectorsDAO;
+import dao.DirectorsGenresDAO;
+import dao.MoviesDAO;
+import dao.MoviesGenresDAO;
+import pojo.Actors;
+import pojo.Directors;
+import pojo.DirectorsGenres;
+import pojo.DirectorsGenresId;
+import pojo.Movies;
+import pojo.MoviesGenres;
+import pojo.MoviesGenresId;
+import util.HibernateSessionFactory;
+
+public class TestActorsDAO {
+
+	public static void main(String[] args) {
+
+		ActorsDAO actorsDAO = new ActorsDAO();
+		Actors actors = actorsDAO.findById(3l);
+		System.out.println(actors.getFirstName());
+
+	}
+}
